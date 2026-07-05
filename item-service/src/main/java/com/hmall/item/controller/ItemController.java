203ddc0880692433a8fc5cba40fdb20b8c6478ea
songweijian <350198902@qq.com>
2026-07-05 12:25:12 +0800

    heima商城项目初始化

---
+++ item-service/src/main/java/com/hmall/item/controller/ItemController.java
@@ -0,0 +1,79 @@
+package com.hmall.item.controller;
+
+
+import com.hmall.common.domain.PageDTO;
+import com.hmall.common.domain.PageQuery;
+import com.hmall.common.utils.BeanUtils;
+import com.hmall.item.domain.dto.ItemDTO;
+import com.hmall.item.domain.dto.OrderDetailDTO;
+import com.hmall.item.domain.po.Item;
+import com.hmall.item.service.IItemService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.List;
+
+@Api(tags = "商品管理相关接口")
+@RestController
+@RequestMapping("/items")
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final IItemService itemService;
+
+    @ApiOperation("分页查询商品")
+    @GetMapping("/page")
+    public PageDTO<ItemDTO> queryItemByPage(PageQuery query) {
+        var result = itemService.page(query.toMpPage("update_time", false));
+        return PageDTO.of(result, ItemDTO.class);
+    }
+
+    @ApiOperation("根据id批量查询商品")
+    @GetMapping
+    public List<ItemDTO> queryItemByIds(@RequestParam("ids") Collection<Long> ids){
+        return itemService.queryItemByIds(ids);
+    }
+
+    @ApiOperation("根据id查询商品")
+    @GetMapping("{id}")
+    public ItemDTO queryItemById(@PathVariable("id") Long id) {
+        return BeanUtils.copyBean(itemService.getById(id), ItemDTO.class);
+    }
+
+    @ApiOperation("新增商品")
+    @PostMapping
+    public void saveItem(@RequestBody ItemDTO item) {
+        itemService.save(BeanUtils.copyBean(item, Item.class));
+    }
+
+    @ApiOperation("更新商品状态")
+    @PutMapping("/status/{id}/{status}")
+    public void updateItemStatus(@PathVariable("id") Long id, @PathVariable("status") Integer status){
+        var item = new Item();
+        item.setId(id);
+        item.setStatus(status);
+        itemService.updateById(item);
+    }
+
+    @ApiOperation("更新商品")
+    @PutMapping
+    public void updateItem(@RequestBody ItemDTO item) {
+        item.setStatus(null);
+        itemService.updateById(BeanUtils.copyBean(item, Item.class));
+    }
+
+    @ApiOperation("根据id删除商品")
+    @DeleteMapping("{id}")
+    public void deleteItemById(@PathVariable("id") Long id) {
+        itemService.removeById(id);
+    }
+
+    @ApiOperation("批量扣减库存")
+    @PutMapping("/stock/deduct")
+    public void deductStock(@RequestBody List<OrderDetailDTO> items){
+        itemService.deductStock(items);
+    }
+}
---
+++ item-service/src/main/java/com/hmall/item/service/impl/ItemServiceImpl.java
@@ -0,0 +1,34 @@
+package com.hmall.item.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmall.common.exception.BizIllegalException;
+import com.hmall.common.utils.BeanUtils;
+import com.hmall.item.domain.dto.ItemDTO;
+import com.hmall.item.domain.dto.OrderDetailDTO;
+import com.hmall.item.domain.po.Item;
+import com.hmall.item.mapper.ItemMapper;
+import com.hmall.item.service.IItemService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+import java.util.List;
+
+@Service
+public class ItemServiceImpl extends ServiceImpl<ItemMapper, Item> implements IItemService {
+
+    @Transactional
+    public void deductStock(List<OrderDetailDTO> items) {
+        for (OrderDetailDTO item : items) {
+            var i = baseMapper.updateStock(item);
+            if(i<=0){
+                throw new BizIllegalException("库存不足！");
+            }
+        }
+    }
+
+    @Override
+    public List<ItemDTO> queryItemByIds(Collection<Long> ids) {
+        return BeanUtils.copyList(listByIds(ids), ItemDTO.class);
+    }
+}
